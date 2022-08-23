39deab4eae9ceda579815b05ba0d3b58426552ef
Juan Motato <jmotatorest@gmail.com>
2022-08-23 15:55:39 -0500

    se actualiza la Logica

--- src/Controller/Logica.java
+++ src/Controller/Logica.java
@@ -7,6 +7,8 @@ import java.util.List;
 
 public class Logica {
 
+    Car car;
+
 
     public void init() {
         int value = Integer.parseInt(JOptionPane.showInputDialog(this.getMenuText()));
@@ -17,7 +19,7 @@ public class Logica {
     private void selectMenuItem(int value) {
         switch (value){
             case 1:
-                System.out.println("sisas");
+                registerCar();
                break;
             case 2:
                 System.out.println("sisas2");
@@ -30,5 +32,20 @@ public class Logica {
         return "Bienvenido al Parkin lot \n" +
                 "1. Registrar auto \n" +
                 "2. Salida auto";
+    }
+
+    private void registerCar(){
+        String idCar="";
+        String color="";
+
+        try {
+
+            idCar=JOptionPane.showInputDialog("Ingrese la placa del vehículo");
+            color=JOptionPane.showInputDialog("Ingrese el color del vehículo");
+
+        } catch (NumberFormatException e) {
+
+        }
+
     }
 }
